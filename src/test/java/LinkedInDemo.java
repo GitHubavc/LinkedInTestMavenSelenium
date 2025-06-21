69a03c50abd2d21e0f0d0aa45cb9d8e3803daf2b
amitGitHubavc <amitvchandran@gmail.com>
2025-06-21 06:31:26 +0530

    First commit1

--- src/test/java/LinkedInDemo.java
+++ src/test/java/LinkedInDemo.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,17 +13,33 @@ public class LinkedInDemo {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.linkedin.com");
         driver.manage().window().maximize();
         driver.findElement(By.xpath("//a[normalize-space()='Sign in']")).click();
-        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("amitvchandran@gmail.com");
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("An@kulam01");
+        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("username@gmail.com");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("$^$^%$**^&");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
         String pageTitle = driver.getTitle();
         System.out.println("The Title of the page is : " + pageTitle);
         Assert.assertTrue(pageTitle.contains("LinkedIn"));
-        driver.quit();
+
+        driver.findElement(By.xpath("//span[@title='Jobs']")).click();
+        driver.findElement(By.xpath("//span[text()='Show all']")).click();
+
+
+
+        driver.findElement(By.xpath("//button[@id='jobs-apply-button-id']")).click();
+        Thread.sleep(3000);
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        driver.findElement(By.xpath("//span[text()='Next']")).click();
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        driver.findElement(By.xpath("//span[text()='Next']")).click();
+
+
+
+
     }
 }
